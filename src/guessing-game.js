fd6a70764fcc34313b3c532d59379153e9775efe
Yuriy Biakov <gbyackov@gmail.com>
2024-09-28 22:19:37 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+        this.minimum = 0;
+        this.maximum = 0;
+        this.lastAnswer = 0;
+    }
 
     setRange(min, max) {
-
+        this.minimum = min;
+        this.maximum = max;
     }
 
     guess() {
-
+        this.lastAnswer = this.minimum + Math.ceil((this.maximum - this.minimum) / 2);
+        return this.lastAnswer;
     }
 
     lower() {
-
+        this.maximum = this.lastAnswer;
     }
 
     greater() {
-
-    }
+        this.minimum = this.lastAnswer;
+        }
 }
 
 module.exports = GuessingGame;
